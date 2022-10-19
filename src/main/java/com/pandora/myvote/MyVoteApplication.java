555ce6ff8ae51a343914837c9e94fce91b46bbf8
wangshouliang <wangshouliang@xiaomi.com>
2022-10-19 09:48:26 +0800

    初始化项目：完成项目构建，依赖等，配置数据库

---
+++ src/main/java/com/pandora/myvote/MyVoteApplication.java
@@ -0,0 +1,13 @@
+package com.pandora.myvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyVoteApplication.class, args);
+    }
+
+}
